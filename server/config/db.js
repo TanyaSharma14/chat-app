15c9b8295247b6b98c37593656b7214b9ec8cf5f
tanya sharma <92660835+TanyaSharma14@users.noreply.github.com>
2026-04-21 01:21:17 +0530

    Add comments to db.js for clarity
    Added comments to clarify the purpose of the mongoose connection and the connectDB function.

--- server/config/db.js
+++ server/config/db.js
@@ -1,13 +1,13 @@
-const mongoose = require('mongoose');
-
-const connectDB = async () => {
+const mongoose = require('mongoose'); //purpose of making this file is to open the MongoDB connection by importing mongoose
+//Mongoose opens the db connection
+const connectDB = async () => { // this is a reusable async bootstrap function
     try {
         await mongoose.connect(process.env.MONGO_URI);
-        console.log('MongoDB Connected...');
-    } catch (err) {
+        console.log('MongoDB Connected...'); // log the success
+    } catch (err) { // in the case of an error, log and exit process
         console.error(err.message);
         process.exit(1);
     }
 };
 
-module.exports = connectDB;
+module.exports = connectDB; //
