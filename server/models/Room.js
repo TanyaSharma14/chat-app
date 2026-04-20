e6eddc86e42571ce79d2454009ab2eb0b5f9505b
tanya sharma <92660835+TanyaSharma14@users.noreply.github.com>
2026-04-21 01:54:40 +0530

    Add comments to RoomSchema for clarity

--- server/models/Room.js
+++ server/models/Room.js
@@ -1,6 +1,8 @@
+// a room is acting like a container where messages happen
+
 const mongoose = require('mongoose');
 
-const RoomSchema = new mongoose.Schema({
+const RoomSchema = new mongoose.Schema({ // defining how to room document will look like
     name: {
         type: String,
         required: false // Only required for group chats
@@ -10,7 +12,7 @@ const RoomSchema = new mongoose.Schema({
         default: false
     },
     participants: [{
-        type: mongoose.Schema.Types.ObjectId,
+        type: mongoose.Schema.Types.ObjectId, // this part stores who is a part of the room(an array of userids)
         ref: 'User'
     }],
     lastMessage: {
